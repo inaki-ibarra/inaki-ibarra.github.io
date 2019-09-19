72e41347c78e3f23f38bc61894f6b74a20a7bc7d
Inaki Ibarra <16115640+ibarrain@users.noreply.github.com>
2019-09-19 14:59:54 +0800

    Add full-stack title

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   let typed = new Typed("#job-title", {
-    strings: ["Software Developer", "Web Developer", "Software Engineer", "Programmer", "Backend Developer"],
+    strings: ["Software Developer", "Web Developer", "Software Engineer", "Programmer", "Backend Developer", "Full-stack Developer"],
     typeSpeed: 80,
     loop: true
   });
